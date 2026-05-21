faef29493f412712d0f20b039d7c3b76c90af1eb
magicrock2007-hue <magicrock2007@gmail.com>
2026-05-21 13:00:46 -0400

    7676

--- src/Coordinates.java
+++ src/Coordinates.java
@@ -6,18 +6,18 @@ public class Coordinates extends GameObject {
     Polygon game;
 
     public Coordinates(Polygon game) {
-        this.game = game;
+        this.game = game; //hi this is a test test test
         setSize(1000, 1000);  // size of the text area
     }
 
     public void act() {
         // reposition every tick so text stays in corner
         repaint();  // redraws this object every tick
-        setLocation(10, 10);
+        setLocation(10, 10);// position in the top-left corner
         
     }
 
-    public void paint(Graphics g) {wfwfwfw
+    public void paint(Graphics g) {
         g.setColor(Color.WHITE);
         g.drawString("X:" + game.getMouseX() + "  Y:" + game.getMouseY(), 0, 20);
         double angle = game.getAngle(game.player.getX(), game.player.getY(),game.getMouseX(), game.getMouseY()) * (180/Math.PI);
