96dc8d2648f416775f6b1f92b5d63450d678286d
Krapath <hugotoisawesome@gmail.com>
2026-05-21 18:45:52 -0400

    wr

--- src/Coordinates.java
+++ src/Coordinates.java
@@ -1,35 +1,23 @@
 import java.awt.Color;
-import java.awt.Graphics; 
+import java.awt.Graphics;
 
 public class Coordinates extends GameObject {
 
     Polygon game;
 
     public Coordinates(Polygon game) {
-        this.game = game; //hi this is a test test test
-        setSize(1000, 1000);  // size of the text area
+        this.game = game;
+        setSize(200, 30);  // size of the text area
     }
 
     public void act() {
         // reposition every tick so text stays in corner
         repaint();  // redraws this object every tick
-        setLocation(10, 10);// position in the top-left corner
-        
+        setLocation(10, 10);
     }
 
     public void paint(Graphics g) {
         g.setColor(Color.WHITE);
         g.drawString("X:" + game.getMouseX() + "  Y:" + game.getMouseY(), 0, 20);
-        double angle = game.getAngle(game.player.getX(), game.player.getY(),game.getMouseX(), game.getMouseY()) * (180/Math.PI);
-        angle =-angle;
-        if (angle < 0){
-        	angle+=360;
-        }
-        g.drawString("Angle:" + angle , 0, 30);
-        g.setColor(Color.RED);
-
-        g.drawRect(game.getMouseX(),game.getMouseY(),4,4);        
-      
-
     }
 }
