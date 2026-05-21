50190e4d41d8e07b8e78fa7a5ae458c38e06cf08
Krapath <hugotoisawesome@gmail.com>
2026-05-21 12:30:10 -0400

    Update Coordinates.java

--- src/Coordinates.java
+++ src/Coordinates.java
@@ -7,17 +7,29 @@ public class Coordinates extends GameObject {
 
     public Coordinates(Polygon game) {
         this.game = game;
-        setSize(200, 30);  // size of the text area
+        setSize(1000, 1000);  // size of the text area
     }
 
     public void act() {
         // reposition every tick so text stays in corner
         repaint();  // redraws this object every tick
         setLocation(10, 10);
+        
     }
 
     public void paint(Graphics g) {
         g.setColor(Color.WHITE);
         g.drawString("X:" + game.getMouseX() + "  Y:" + game.getMouseY(), 0, 20);
+        double angle = game.getAngle(game.player.getX(), game.player.getY(),game.getMouseX(), game.getMouseY()) * (180/Math.PI);
+        angle =-angle;
+        if (angle < 0){
+        	angle+=360;
+        }
+        g.drawString("Angle:" + angle , 0, 30);
+        g.setColor(Color.RED);
+
+        g.drawRect(game.getMouseX(),game.getMouseY(),4,4);        
+      
+
     }
 }
