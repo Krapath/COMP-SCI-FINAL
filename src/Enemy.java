6683f5a58b9f511f7db97c3e848cb8489cd012a1
Zerox3228 <132528139+Zerox3228@users.noreply.github.com>
2026-05-25 17:39:11 -0400

    enemy collision and no enemy spawn on each other.

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,8 +1,11 @@
-import java.util.Random;
+
 import java.awt.Color;
+import java.util.Random;
+
 public class Enemy extends GameObject {
+
     Random r = new Random();
-    public int size  = 25;
+    public int size = 25;
     public int speed = 2;
     public int health = 3;
     public int enemyDamage = 1;
@@ -11,35 +14,67 @@ public class Enemy extends GameObject {
     Polygon game;
 
     public Enemy(Polygon game) {
-        
+
         this.game = game;
         setSize(size, size);
         setColor(Color.GREEN);
         int x;
         int y;
+        boolean collided = false;
         do {
-            x = r.nextInt(game.getWindowWidth()-size);
-            y = r.nextInt(game.getWindowHeight()-size);
-        } while ((Math.abs(game.player.getX() - x) < (game.getWindowWidth()+game.getWindowHeight())/100) || (Math.abs(game.player.getY() - y) < (game.getWindowWidth()+game.getWindowHeight())/100)); // ensures that the enemy does not too close to the player
+            do {
+                x = r.nextInt(game.getWindowWidth() - size);
+                y = r.nextInt(game.getWindowHeight() - size);
+            } while ((Math.abs(game.player.getX() - x) < (game.getWindowWidth() + game.getWindowHeight()) / 100) || (Math.abs(game.player.getY() - y) < (game.getWindowWidth() + game.getWindowHeight()) / 100)); // ensures that the enemy does not too close to the player
+
+            //this code does not work with enemies of different sizes. Fix if variable enemy sizes.
+            for (int i = 0; i < game.enemies.size(); i++) { //ensures enemies dont spawn directly on top of each other
+                Enemy other = game.enemies.get(i);
+                if (other != this && other.getY() > y + 25 && other.getY() < y && other.getX() > x + 25 && other.getX() < x) {
+                    collided = true;
+                }
+            }
+        } while (collided); //permits the spawn location if not on top of another enemy.
 
         setLocation(x, y);
+
     }
 
     public void act() {
-        if (game.choosingBuff) return;
+        if (game.choosingBuff) {
+            return;
+        }
         int x = getX();
         int y = getY();
+        boolean collided = false;
         int playerX = game.player.getX();
         int playerY = game.player.getY();
         double playerAngle = game.getAngle(x, y, playerX, playerY);
-        x += (int)(Math.cos(playerAngle) * speed);
-        y += (int)(Math.sin(playerAngle) * speed);
+
+        for (int i = 0; i < game.enemies.size(); i++) {
+            Enemy other = game.enemies.get(i);
+
+            if (collides(other) && other != this) { //if touching another enemy, moves this enemy away from the other one.
+                int enemyX = game.enemies.get(i).getX();
+                int enemyY = game.enemies.get(i).getY();
+                double enemyAngle = game.getAngle(x, y, enemyX, enemyY);
+                x -= (int) (Math.cos(enemyAngle) * 1.5 * speed);
+                y -= (int) (Math.sin(enemyAngle) * 1.5 * speed);
+                collided = true;
+                break;
+            }
+        }
+        if (!collided) { //if not touching another enemy, chase player.
+            x += (int) (Math.cos(playerAngle) * speed);
+            y += (int) (Math.sin(playerAngle) * speed);
+        }
+
         setX(x);
         setY(y);
 
-        displayOld+=1;
-		if (displayOld > 60) {	
-			displayOld = 0;
+        displayOld += 1;
+        if (displayOld > 60) {
+            displayOld = 0;
             setColor(Color.ORANGE);
         }
 
--- src/Polygon.java
+++ src/Polygon.java
@@ -5,72 +5,73 @@
  * End date: 
  * Project: Polygons - a roguelike top-down shooter.
  */
-
 import java.util.Random;
 import java.util.ArrayList;
+
 public class Polygon extends Game {
-	Random r = new Random();
-	Player player;
-	Projectile projectile;
-	Enemy enemy;
-	public boolean choosingBuff = false;
-	ArrayList<Enemy> enemies = new ArrayList<Enemy>();
-	ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
-	ArrayList<XpOrb> xpOrbs = new ArrayList<XpOrb>();
-	ArrayList<PowerUp> powerUps = new ArrayList<PowerUp>();
 
-	public void setup() {
-		setDelay(16); // 60fps
-		setTitle("Polygon");
-		player = new Player(this);
-		player.setLocation(335, 225); // middle
-		add(player);
-		add(new DisplayDebug(this));
+    Random r = new Random();
+    Player player;
+    Projectile projectile;
+    Enemy enemy;
+    public boolean choosingBuff = false;
+    ArrayList<Enemy> enemies = new ArrayList<Enemy>();
+    ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
+    ArrayList<XpOrb> xpOrbs = new ArrayList<XpOrb>();
+    ArrayList<PowerUp> powerUps = new ArrayList<PowerUp>();
 
-	}
+    public void setup() {
+        setDelay(16); // 60fps
+        setTitle("Polygon");
+        player = new Player(this);
+        player.setLocation(335, 225); // middle
+        add(player);
+        add(new DisplayDebug(this));
 
+    }
 
-	public void act() {
-		if (choosingBuff) return; // pause the game while choosing a buff(lock the game)
-		
-		if (mouseLeftPressed()) {
-			if (player.attackDelay > 5) {	
-				player.attackDelay = 0;
-				projectile = new Projectile(this);
-				add(projectile);
-				projectiles.add(projectile);
-			}
-		}
+    public void act() {
+        if (choosingBuff) {
+            return; // pause the game while choosing a buff(lock the game)
+        }
+        if (mouseLeftPressed()) {
+            if (player.attackDelay > 5) {
+                player.attackDelay = 0;
+                projectile = new Projectile(this);
+                add(projectile);
+                projectiles.add(projectile);
+            }
+        }
 
-		if (r.nextInt(300) < 20) { // 0.33% chance each tick to spawn an enemy
-			enemy = new Enemy(this);
-			add(enemy);
-			enemies.add(enemy);
-		}
-		if (player.health <= 0) { //stops if dies
-			stopGame();
-		}
+        if (r.nextInt(300) < 20) { // 0.33% chance each tick to spawn an enemy
+            enemy = new Enemy(this);
+            add(enemy);
+            enemies.add(enemy);
+        }
+        if (player.health <= 0) { //stops if dies
+            stopGame();
+        }
 
-		if (player.score >= 5) { // spawns a powerup when the player reaches level 5
-			choosingBuff = true;
-			int centeredY = getWindowHeight() / 2 - getWindowHeight() / 6;
-			powerUps.add(new PowerUp(getWindowWidth() / 4     - getWindowWidth() / 10, centeredY, this));
-			powerUps.add(new PowerUp(getWindowWidth() / 4 * 2 - getWindowWidth() / 10, centeredY, this));
-			powerUps.add(new PowerUp(getWindowWidth() / 4 * 3 - getWindowWidth() / 10, centeredY, this));
-			for (PowerUp p : powerUps) {
-				add(p);
-			}
-			player.score = 0; // reset score after spawning powerup
+        if (player.score >= 5) { // spawns a powerup when the player reaches level 5
+            choosingBuff = true;
+            int centeredY = getWindowHeight() / 2 - getWindowHeight() / 6;
+            powerUps.add(new PowerUp(getWindowWidth() / 4 - getWindowWidth() / 10, centeredY, this));
+            powerUps.add(new PowerUp(getWindowWidth() / 4 * 2 - getWindowWidth() / 10, centeredY, this));
+            powerUps.add(new PowerUp(getWindowWidth() / 4 * 3 - getWindowWidth() / 10, centeredY, this));
+            for (PowerUp p : powerUps) {
+                add(p);
+            }
+            player.score = 0; // reset score after spawning powerup
 
-		}
+        }
 
-	}
+    }
 
-	public static void main(String[] args) {
-		Polygon game = new Polygon();//	
-		game.setVisible(true);
-		game.initComponents();
-		MainMenu menu = new MainMenu(game);//comment this out if you don't want the main menu to pop up
-		menu.setUp();//comment this out if you don't want the main menu to pop up
-	}
+    public static void main(String[] args) {
+        Polygon game = new Polygon();//	
+        game.setVisible(true);
+        game.initComponents();
+        MainMenu menu = new MainMenu(game);//comment this out if you don't want the main menu to pop up
+        menu.setUp();//comment this out if you don't want the main menu to pop up
+    }
 }
