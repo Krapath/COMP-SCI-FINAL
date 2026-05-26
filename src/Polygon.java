5a0d6393c4963096a980c68f0f9e04ddbda4e5a6
Zerox3228 <132528139+Zerox3228@users.noreply.github.com>
2026-05-26 19:45:54 -0400

    pierce on bullets

--- src/Polygon.java
+++ src/Polygon.java
@@ -5,7 +5,6 @@
  * End date: 
  * Project: Polygons - a roguelike top-down shooter.
  */
-
 import java.util.Random;
 
 import javax.swing.ImageIcon; //remove later
@@ -21,7 +20,7 @@ public class Polygon extends Game {
     Enemy enemy;
     public boolean choosingBuff = false;
     ArrayList<Enemy> enemies = new ArrayList<Enemy>();
-    ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
+    public ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
     ArrayList<XpOrb> xpOrbs = new ArrayList<XpOrb>();
     ArrayList<PowerUp> powerUps = new ArrayList<PowerUp>();
 
@@ -36,7 +35,6 @@ public class Polygon extends Game {
         ImageIcon test = new ImageIcon("8.webp");
         //getContentPane().setBackground(test);//remove later
 
-
     }
 
     public void act() {
@@ -44,7 +42,7 @@ public class Polygon extends Game {
             return; // pause the game while choosing a buff(lock the game)
         }
         if (mouseLeftPressed()) {
-            if (player.attackDelay > 5) {
+            if (player.attackDelay > 10) {
                 player.attackDelay = 0;
                 projectile = new Projectile(this);
                 add(projectile);
@@ -52,7 +50,7 @@ public class Polygon extends Game {
             }
         }
 
-        if (r.nextInt(300) < 20) { // 0.33% chance each tick to spawn an enemy
+        if (r.nextInt(300) < 5) { // 0.33% chance each tick to spawn an enemy
             enemy = new Enemy(this);
             add(enemy);
             enemies.add(enemy);
--- src/Projectile.java
+++ src/Projectile.java
@@ -1,4 +1,7 @@
+
 import java.awt.Color;
+import java.util.ArrayList;
+
 public class Projectile extends GameObject {
 
     Polygon game;
@@ -6,24 +9,29 @@ public class Projectile extends GameObject {
     double yVel;
     int distanceTraveled = 0;
     int damage = 1;
+    int pierceCount = 3;
+    ArrayList<Enemy> hitEnemies = new ArrayList<Enemy>();
 
     public Projectile(Polygon game) {
         this.game = game;
-        setLocation(game.player.getX()+game.player.size/3, game.player.getY()+game.player.size/3); // update position
+        setLocation(game.player.getX() + game.player.size / 3, game.player.getY() + game.player.size / 3); // update position
         setSize(10, 10); // size of the projectile
         setColor(Color.YELLOW);
-        if (game.mouseLeftPressed()){
-                double angle = game.getAngle(game.player.getX()+game.player.size/2, game.player.getY()+game.player.size/2,game.getMouseX(), game.getMouseY());
-                double speed = 20; // adjust as needed
-                xVel = speed * Math.cos(angle);
-                yVel = speed * Math.sin(angle);
-        }   
+        if (game.mouseLeftPressed()) {
+            double angle = game.getAngle(game.player.getX() + game.player.size / 2, game.player.getY() + game.player.size / 2, game.getMouseX(), game.getMouseY());
+            double speed = 20; // adjust as needed
+            xVel = speed * Math.cos(angle);
+            yVel = speed * Math.sin(angle);
+        }
     }
 
-    public void act() { 
-        if (game.choosingBuff) return;// projectiles do not move or collide with enemies while the player is choosing a buff
+    public void act() {
+        if (game.choosingBuff) {
+            return;// projectiles do not move or collide with enemies while the player is choosing a buff
+
+        }
         distanceTraveled += Math.sqrt(xVel * xVel + yVel * yVel); // update distance traveled
-        setLocation(getX() + (int)xVel, getY() + (int)yVel); // update position
+        setLocation(getX() + (int) xVel, getY() + (int) yVel); // update position
 
         if (distanceTraveled > 400) { // remove projectile after it has traveled a certain distance
             game.remove(this);
@@ -32,32 +40,50 @@ public class Projectile extends GameObject {
 
         for (int i = 0; i < game.enemies.size(); i++) {
             if (collides(game.enemies.get(i))) {
-                game.enemies.get(i).health -= damage; // reduce enemy health on collision
+                boolean hit = false;
 
-                game.enemies.get(i).setColor(Color.RED);
-
-                int enemyX = game.enemies.get(i).getX();
-                int enemyY = game.enemies.get(i).getY();
-                if (game.enemies.get(i).health <= 0) {
-
-
-                    XpOrb xp = new XpOrb(enemyX, enemyY, game); // create an xp orb at the location of the defeated enemy
-                    game.add(xp);// add the xp orb to the game
-                    game.xpOrbs.add(xp); // add the xp orb to the list
-                    
-                    game.remove(game.enemies.get(i)); // remove enemy if health is depleted
-                    game.enemies.remove(i); // remove enemy from the list
+                for (int j = 0; j < hitEnemies.size(); j++) { //if enemy already hit, dont hit again.
+                    if (game.enemies.get(i) == hitEnemies.get(j)) {
+                        hit = true;
+                    }
                 }
 
-                game.remove(this); // remove projectile after it hits an enemy
-                game.projectiles.remove(this); // remove projectile from the list
-                break; // exit loop after collision
+                if (!hit) { //if not hit
+                    hitEnemies.add(game.enemies.get(i)); //count as hit from now on
+                    pierceCount--;
+                    game.enemies.get(i).health -= damage; // reduce enemy health on collision
+
+                    game.enemies.get(i).setColor(Color.RED);
+
+                    int enemyX = game.enemies.get(i).getX();
+                    int enemyY = game.enemies.get(i).getY();
+
+                    if (game.enemies.get(i).health <= 0) {
+
+                        XpOrb xp = new XpOrb(enemyX, enemyY, game); // create an xp orb at the location of the defeated enemy
+                        game.add(xp);// add the xp orb to the game
+                        game.xpOrbs.add(xp); // add the xp orb to the list
+
+                        //if (hitEnemies.contains(game.enemies.get(i))) {
+                        hitEnemies.remove(game.enemies.get(i));
+                        // }
+
+                        game.remove(game.enemies.get(i)); // remove enemy if health is depleted
+                        game.enemies.remove(i); // remove enemy from the list
+
+                    }
+                }
+
+                if (pierceCount == 0) {
+                    game.remove(this); // remove projectile after it hits an enemy
+                    game.projectiles.remove(this); // remove projectile from the list
+                    break; // exit loop after collision
+                }
+
             }
         }
-  
 
         // move the projectile according to its velocity
-
     }
 
 }
