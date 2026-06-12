7b960118d07629c98d40eba469c700a853a51f37
magicrock2007-hue <magicrock2007@gmail.com>
2026-06-12 18:27:46 -0400

    commented for spawn animation

--- src/SpawnAnimation.java
+++ src/SpawnAnimation.java
@@ -4,33 +4,38 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class SpawnAnimation extends GameObject {
-
-    // set variables
+    // array list holding all the objects
+    private static ArrayList<SpawnAnimation> spawnParticles = new ArrayList<>();
+    // set variables for dummy constructor
     static boolean spawningAnimation = false;
     PolygonGame game;
     int size;
+    // intialize object variables
     public int lifespan;
     public int particleTransparency = 10;
-    static Random r = new Random();
     public static int playerTransparency = 0;
     static boolean playSpawnSound = true;
 
+    // make random object
+    static Random r = new Random();
 
-    private static ArrayList<SpawnAnimation> spawnParticles = new ArrayList<>();
-
-    // dummy constructor for the spawn particles, not used to actually spawn the
-    // animation
+    /**
+     * create dummy constructor for spawn particles
+     */
     public SpawnAnimation(PolygonGame game, int x, int y) {
+        // set value for object variables
         this.game = game;
         size = Player.size / 2;
         setSize(size, size);
         setPosition(x, y);
         setColor(new Color(0, 0, 255, particleTransparency));
         lifespan = r.nextInt(10) + 10;
-        spawnParticles.add(this); //extra cube 
+        spawnParticles.add(this); // extra cube
     }
 
-    // method to spawn particles
+    /**
+     * the method used to spawn the particles for the cube spawning animation
+     */
     public void spawnAnimation(PolygonGame game) {
         int w = game.getWindowWidth();
         int h = game.getWindowHeight();
@@ -49,25 +54,27 @@ public class SpawnAnimation extends GameObject {
         addParticle(game, (w + size) / 2, h + size); // Bottom Middle
         addParticle(game, w + size, h + size); // Bottom Right
 
-        spawningAnimation = true;
-        if (playSpawnSound){
-            SoundEffects("SFX/SPAWN_SOUND.wav",-4.0f);
-            playSpawnSound=false;
+        // play spawning sound effect
+        if (playSpawnSound) {
+            SoundEffects("SFX/SPAWN_SOUND.wav", -4.0f);
+            playSpawnSound = false;
         }
+        spawningAnimation = true;
     }
 
-    // helper method to add a particle to the game and the list
+    /**
+     * helper method to add a particle to the game and the list
+     */
     private void addParticle(PolygonGame game, int x, int y) {
         SpawnAnimation particle = new SpawnAnimation(game, x, y);
         game.add(particle);
         spawnParticles.add(particle);
-        
+
     }
 
     public void act() {
-        // optimize
         if (!spawningAnimation) {
-            return;
+            return; // skips over all the code if not during the spawning animation
         }
         int speed = (game.getWindowWidth() + game.getWindowHeight()) / 100; // speed of the particles
         // get particle position
@@ -79,23 +86,23 @@ public class SpawnAnimation extends GameObject {
         x += (Math.cos(playerAngle) * speed) + (r.nextInt(speed * 2) - speed);
         y += (Math.sin(playerAngle) * speed) + (r.nextInt(speed * 2) - speed);
         setPosition(x, y);
-        if (collides(game.player)) { // if collides with player, remove from game and list
+        if (collides(game.player)) { // if collides with player reduce lifespan
             lifespan--;
             if (lifespan <= 0) {
-                game.remove(this);
+                game.remove(this); // if lifespan hits zero remove particle
                 spawnParticles.remove(this);
                 playerTransparency += (int) (255 / 8 - 10); // increase player transparency
-                game.player.setColor(new Color(0, 0, 255, playerTransparency));//Pretty sure the code bricks here if the player is shot while spawning
+                game.player.setColor(new Color(0, 0, 255, playerTransparency));// Pretty sure the code bricks here if
+                                                                               // the player is shot while spawning
             }
         }
-        if (particleTransparency < 250) { // fade in
+        if (particleTransparency < 250) { // make particles fade it
             particleTransparency += 15;
             setColor(new Color(0, 0, 255, particleTransparency));
         }
 
-        if (spawnParticles.size() <= 1) { // a few particles are left make them disappear
+        if (spawnParticles.size() <= 1) {
             // ensures that all particles are removed from the game
-
             for (SpawnAnimation m : spawnParticles) {
                 game.remove(m);
             }
@@ -103,7 +110,7 @@ public class SpawnAnimation extends GameObject {
             game.remove(this);
             // set the player to full color
             game.player.setColor(new Color(0, 0, 255, 255));
-            
+            // turn off the spawning animation
             spawningAnimation = false;
         }
     }
